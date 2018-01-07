5c6923c3e0b464065907ab77d90b319072376d86
MagicBowen <e.bowen.wang@icloud.com>
2018-01-07 17:27:52 +0800

    first commit

---
+++ server.js
@@ -0,0 +1,56 @@
+const Koa = require('koa');
+const fs = require('mz/fs');
+const router = require('koa-router')();
+const app = new Koa();
+
+
+// log request URL:
+app.use(async (ctx, next) => {
+    console.log(`process request for '${ctx.request.method} ${ctx.request.url}' ...`);
+    var start = new Date().getTime();
+    await next();
+    var execTime = new Date().getTime() - start;
+    ctx.response.set('X-Response-Time', `${execTime}ms`); 
+    console.log(`... response in duration ${execTime}ms`);
+});
+
+router.get('/', async (ctx, next) => {
+    ctx.response.body = `<h1>Welcome to westore!</h1>`;
+});
+
+async function fillImageToCtx(ctx, image) {
+    if (await fs.exists(image)) {
+        ctx.response.type = "image/jpg";
+        ctx.response.body = await fs.readFile(image);
+    } else {
+        ctx.response.status = 404;
+    }    
+}
+
+router.get('/image/:name', async (ctx, next) => {
+    var name = ctx.params.name;
+    var image = `./static/pictures/images/${name}`;
+    await fillImageToCtx(ctx, image);
+});
+
+router.get('/size/image', async (ctx, next) => {
+    var relation = ctx.query.relation;
+    var sex = ctx.query.sex;
+    var type = ctx.query.type;
+    var image = `./static/pictures/size_relation/${sex}/${type}/${relation}`;
+    await fillImageToCtx(ctx, image);
+});
+
+router.get('/product/image', async (ctx, next) => {
+    var productId = ctx.query.product;
+    var pictureName = ctx.query.picture;
+    var image = `./static/pictures/products/${productId}/target/${pictureName}`;
+    await fillImageToCtx(ctx, image);
+});
+
+
+// add router
+app.use(router.routes());
+
+app.listen('0.0.0.0', 8080);
+console.log('server started at 0.0.0.0:8080...');
